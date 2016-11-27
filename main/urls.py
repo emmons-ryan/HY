b13f700c67969176e70c00c3181fccd80e7ca994
emmons-ryan <emmons.ryan@gmail.com>
2016-11-27 15:40:43 -0500

    added detail views

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
+    #blog url
     url(r'^blog/$', views.blog),
+    # blog posts url
+    url(r'^blog/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     ]
